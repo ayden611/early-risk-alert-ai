f9e137ab40405a00ef5288ef26da883f2c8ae344
Milton Munroe <53419619+ayden611@users.noreply.github.com>
2026-02-17 11:42:45 -0500

    Update demo_model.py

--- demo_model.py
+++ demo_model.py
@@ -4,6 +4,8 @@ import numpy as np
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import os
+import joblib
 
 # Feature matrix: [Age, BMI, Exercise Level]
 X = np.array([
@@ -43,11 +45,11 @@ print("\nClassification Report:\n", classification_report(y_test, y_pred))
 new_patient = np.array([[50, 32, 0]])
 prediction = model.predict(new_patient)
 probability = model.predict_proba(new_patient)
-import jpblib
-
-joblib.dump(model, 'demo_model.pkl")
-            print("model saved as demo_model.pkl")
 
 print("\nNew Patient Prediction (0=Low Risk, 1=High Risk):", prediction[0])
 print("Probability:", probability)
+
+# saved trained model
+joblib.dump(model, "demo_model.pkl")
+print("model saved as demo_model.pkl")
 
