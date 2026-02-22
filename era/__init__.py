0775793ee5f0253d6300d7ebfac3cba557914d9c
Milton Munroe <53419619+ayden611@users.noreply.github.com>
2026-02-22 07:36:51 -0500

    Update __init__.py

--- era/__init__.py
+++ era/__init__.py
@@ -7,15 +7,16 @@ from .web.routes import web_bp
 
 
 def create_app():
-    app = Flask(__name__, template_folder="templates", static_folder="static")
+    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    templates_dir = os.path.join(base_dir, "templates")
+    static_dir = os.path.join(base_dir, "static")
+
+    app = Flask(__name__, template_folder=templates_dir, static_folder=static_dir)
     app.config.from_object(Config)
 
     db.init_app(app)
 
-    # Register API
     app.register_blueprint(api_bp, url_prefix="/api/v1")
-
-    # Register Web (UI pages)
     app.register_blueprint(web_bp)
 
     with app.app_context():
