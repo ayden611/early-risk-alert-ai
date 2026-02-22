f1b0176cb2ad62d3ffa618efc7846a8a5ada2c58
Milton Munroe <53419619+ayden611@users.noreply.github.com>
2026-02-22 06:47:16 -0500

    Update routes.py

--- era/web/routes.py
+++ era/web/routes.py
@@ -5,3 +5,6 @@ web_bp = Blueprint("web", __name__)
 @web_bp.get("/")
 def home():
     return render_template("index.html")
+@web_bp.get("/history")
+def history()
+    return render_template("history.html")
