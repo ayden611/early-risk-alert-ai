6ced401adac0703edea8402b25c848ccd292db56
Milton Munroe <53419619+ayden611@users.noreply.github.com>
2026-02-22 06:20:49 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,3 @@
-import os
-import sys
-
-# Force Python to load local project folder first
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-if BASE_DIR not in sys.path:
-    sys.path.insert(0, BASE_DIR)
-
 from era import create_app
 
 app = create_app()
