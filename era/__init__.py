d3a1497ff44dc8bd9d2d92ee981299f211e40ab4
Milton Munroe <53419619+ayden611@users.noreply.github.com>
2026-03-08 10:01:04 -0400

    Update __init__.py

--- era/__init__.py
+++ era/__init__.py
@@ -8,7 +8,7 @@ from .web.routes import web_bp
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="../templates")
     app.config.from_object(Config)
 
     db.init_app(app)
