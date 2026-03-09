4e4fd1b12ed714476d137aac73fce11395598ffd
Milton Munroe <53419619+ayden611@users.noreply.github.com>
2026-03-09 07:39:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 from era import create_app
+from flask import send_from_directory
+
+@app.route("/pitch-deck")
+def pitch_deck():
+    return send_from_directory(
+        "static",
+        "Early_Risk_Alert_AI_Pitch_Deck.pdf",
+        as_attachment=True
+    )
 
 app = create_app()
