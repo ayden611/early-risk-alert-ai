9ef5a3265733a735f0ed726436f1cfe8503eb5ff
Milton Munroe <53419619+ayden611@users.noreply.github.com>
2026-03-10 11:10:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from era import create_app
+from flask import render_template
 
 app = create_app()
 
+@app.route("/admin")
+@app.route("/investor")
+@app.route("/dashboard")
+
+@app.route("/")
+def home():
+    return render_template("main.html")
