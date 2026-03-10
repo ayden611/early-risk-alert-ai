7ad64406005b7e75c513e9680e561cf4be8eb270
Milton Munroe <53419619+ayden611@users.noreply.github.com>
2026-03-10 11:14:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from flask import render_template
 
 app = create_app()
 
-@app.route("/admin")
-@app.route("/investor")
-@app.route("/dashboard")
-
 @app.route("/")
 def home():
     return render_template("main.html")
