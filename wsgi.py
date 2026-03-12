22af85f29a6181c04075b252df39b67d63d8c0c5
Milton Munroe <53419619+ayden611@users.noreply.github.com>
2026-03-12 14:16:39 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,7 @@
-from era import create_app
-
-app = create_app()
+try:
+    from era import create_app
+    app = create_app()
+    print("WSGI APP:", app, type(app), flush=True)
+except Exception as e:
+    print("WSGI IMPORT FAILED:", repr(e), flush=True)
+    raise
