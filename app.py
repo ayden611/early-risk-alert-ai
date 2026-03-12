99cc065f9b4fd60ad20c1a4b5fa3bcea738d6817
Milton Munroe <53419619+ayden611@users.noreply.github.com>
2026-03-12 12:29:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from era import create_app
 
-app = create_app()
-
+try:
+    app = create_app()
+    print("APP CREATED OK:", app)
+except Exception as e:
+    print("CREATE_APP FAILED:", repr(e))
+    raise
